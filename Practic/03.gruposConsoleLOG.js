0d693d7802e0839659cea014b05f7270a6f7bdf4
sectario <trcesarcamilo@gmail.com>
2021-12-27 18:08:06 -0500

    27dic2021

---
+++ Practic/03.gruposConsoleLOG.js
@@ -0,0 +1,18 @@
+//codigo del cuadrado
+
+console.group("cuadrados");
+    //Está fuincion permite definir grupos de consol.log()
+
+const ladoCuadrado = 5;
+
+console.log(" los lados del cuadrado miden " + ladoCuadrado);
+
+const perimetrocuadrado =ladoCuadrado * 4;
+
+console.log(" El perimetro del cuadrado miden " + perimetrocuadrado);
+console.groupEnd();
+
+//codigo triangulos
+console.group("triangulos");
+console.log("Los trianguloos");
+console.groupEnd();
